e43e213e8a8c97b09c49cc6d2bd83d6cd376a4c4
2320030133 <2320030133@klh.edu.in>
2024-09-10 11:35:41 +0530

    Add files via upload

---
+++ SimpleRideSharing/src/simpleridesharing/PaymentMethod.java
@@ -0,0 +1,19 @@
+package simpleridesharing;
+
+public class PaymentMethod {
+	private String method;
+
+    private PaymentMethod(String method) {
+        this.method = method;
+    }
+
+    public void pay(double amount) {
+        System.out.println("Paying " + amount + " with " + method + ".");
+    }
+
+    public static PaymentMethod createPaymentMethod(String method) {
+        return new PaymentMethod(method);
+    }
+
+}
+
---
+++ SimpleRideSharing/src/simpleridesharing/Vehicle.java
@@ -0,0 +1,19 @@
+package simpleridesharing;
+
+public class Vehicle {
+	private String type;
+
+    private Vehicle(String type) {
+        this.type = type;
+    }
+
+    public void ride() {
+        System.out.println("Riding a " + type + "!");
+    }
+
+    public static Vehicle createVehicle(String type) {
+        return new Vehicle(type);
+    }
+
+}
+
